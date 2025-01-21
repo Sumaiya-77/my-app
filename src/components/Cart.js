4cff9c36612ef351731459ce1c5fea4855189033
Sumaiya-77 <sumaiyamohammed494@gmail.com>
2025-01-21 10:53:00 +0530

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,11 +3,13 @@ import Itemlist from "./Itemlist";
 
 // Cart.jsx
 const Cart = () => {
-const cartItems =  useSelector(store=>store.Cart.items)
-  return 
-  <div>
-    <Itemlist items={cartItems}/>
- </div>;
+  const cartItems = useSelector(store => store.Cart.items);
+
+  return (
+    <div>
+      <Itemlist items={cartItems} />
+    </div>
+  );
 };
 
 export default Cart;
